2fab2b420be223427ece884fde0a70afa02f7995
subash-durairaj-4430 <subash.durairaj@zohocorp.com>
2026-03-25 17:30:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000 // Set your desired port
+  },
   plugins: [
     vue(),
     vueJsx(),
