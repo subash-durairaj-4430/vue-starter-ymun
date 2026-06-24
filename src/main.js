64eb4fae23b3f5546c515366d5c6202d2550d505
subash.durairaj <subash.durairaj@zohocorp.com>
2026-06-24 14:19:41 +0530

    terst login

--- src/main.js
+++ src/main.js
@@ -4,8 +4,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import catalyst from 'zcatalyst-sdk-node'
+
 const app = createApp(App)
 
 app.use(router)
 
 app.mount('#app')
+
+
+catalyst.auth().signIn(document.getElementById("login"));
+
+console.log("Login trying to render");
